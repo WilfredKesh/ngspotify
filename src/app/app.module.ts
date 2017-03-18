86e73d3e2aab63841ea3dc4f10035684b8d9d970
wilfredkesh <wilfredkesh5@gmail.com>
2017-03-18 16:17:24 -0400

    added spotify.service to fetch array of artists fom the spotify api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 import { AppComponent }  from './app.component';
 import { SearchComponent } from './components/search/search.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { routing} from './app.routing';
 
+
 @NgModule({
-  imports:      [ BrowserModule ,routing],
+  imports:      [ BrowserModule ,routing, FormsModule, HttpModule],
   declarations: [ AppComponent, AboutComponent, SearchComponent, NavbarComponent  ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import {SpotifyService} from '../../services/spotify.service';
 
 @Component({
   moduleId: module.id,
   selector: 'search',
   templateUrl: 'search.component.html',
 })
-export class SearchComponent  {  }
+export class SearchComponent  { 
+
+  constructor(private _spotifyService: SpotifyService){}
+
+  searchStr: string;
+  searchMusic(){
+    this._spotifyService.searchSong(this.searchStr).subscribe( res => {
+      console.log(res.artists.items);
+    });
+  }
+ }
